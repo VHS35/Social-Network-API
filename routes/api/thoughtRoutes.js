fa59c57ff138ca29f02e7280898d9555160fa996
VHS35 <110948699+VHS35@users.noreply.github.com>
2023-02-27 22:09:04 -0600

    added routes/controllers

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('controller');//need to make controllers
+
+//  /api/users
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/user/:userId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions').delete(removeReaction);
+
+module.exports = router;
---
+++ routes/api/userRoute.js
@@ -0,0 +1,24 @@
+// used code from miniproject as a guide
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('controller');//need to make controllers
+
+//  /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/user/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend);
+
+router.route('/:userId/friends/:friendId').delete(removeFriend);
+
+module.exports = router;
